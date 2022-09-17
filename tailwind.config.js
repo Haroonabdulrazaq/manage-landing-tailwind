2f8e9243c8a2d94fb105acc24cce1c2ce6b369dc
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-09-17 06:24:11 +0100

    Add Numbered List section

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        gray: 'hsl(225°, 57%, 77%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
